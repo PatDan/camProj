64b0f5bf9598c5344e6a88ef28b648c477510ebe
Anders <anders.buhl94@gmail.com>
2015-11-20 10:38:57 +0100

    Added jars and http stuff and started servers shell

--- src/server/ClientReaderThread.java
+++ src/server/ClientReaderThread.java
@@ -5,9 +5,13 @@ import java.io.BufferedReader;
 public class ClientReaderThread extends Thread {
 	private ServerMonitor monitor;
 	private BufferedReader in;
+	
 	public ClientReaderThread(ServerMonitor serverMonitor, BufferedReader in) {
 		this.monitor = serverMonitor;
 		this.in = in;
 	}
 
+	public void run() {
+		
+	}
 }
--- src/server/ServerMonitor.java
+++ src/server/ServerMonitor.java
@@ -3,17 +3,57 @@ package server;
 import java.io.BufferedReader;
 import java.io.PrintWriter;
 
+import se.lth.cs.eda040.fakecamera.AxisM3006V;
+
 public class ServerMonitor {
+	public static final int MOVIE_MODE = 1;
+	public static final int IDLE_MODE = 2;
+	private int mode;
+	private long lastImage;
+	private AxisM3006V cam;
 
 	public ServerMonitor() {
-
+		mode = IDLE_MODE;
+		lastImage = System.currentTimeMillis() - 5000;
+		cam = new AxisM3006V();
+		cam.init();
+		cam.connect();
 	}
-	
+
 	synchronized void setInput(BufferedReader in) {
 		new ClientReaderThread(this, in).start();
 	}
-	
-	synchronized void setOutput(PrintWriter out)  {
+
+	synchronized void setOutput(PrintWriter out) {
 		new ServerWriterThread(this, out).start();
+	}
+
+	synchronized int mode() {
+		return mode;
+	}
+
+	synchronized byte[] image() {
+		if (mode == IDLE_MODE) {
+			try {
+				long t1;
+
+				while (lastImage + 5000 < (t1 = System.currentTimeMillis()))
+					wait(lastImage + 5000 - t1);
+			} catch (InterruptedException e) {
+				System.out.println("Server interrupted while waiting for image");
+			}
+		}
+		
+		byte[] jpeg = new byte[AxisM3006V.IMAGE_BUFFER_SIZE];
+		cam.getJPEG(jpeg, 0);
+		byte[] time = new byte[AxisM3006V.TIME_ARRAY_SIZE];
+		cam.getTime(time, 0);
+		byte motion = 0;
+		Integer a = jpeg.length + time.length + 1 + Integer.BYTES;
+		byte length = a.byteValue(); 
+		
+		lastImage = System.currentTimeMillis();
+		
+		return new byte[0];
 	}
 }
