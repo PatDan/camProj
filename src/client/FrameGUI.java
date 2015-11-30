6e49bdab7ba2de89ba42e11986cc9dd10fb3e323
Anders Buhl <anders.buhl94@gmail.com>
2015-11-30 17:49:33 +0100

    Added javadoc and Extracted the Main method from FrameGUI
    mikael did this

--- src/client/FrameGUI.java
+++ src/client/FrameGUI.java
@@ -31,7 +31,11 @@ public class FrameGUI extends JFrame {
 	private VideoPanel videoFrame2 = new VideoPanel();
 	private boolean isMode;
 	private ClientMonitor cm;
-
+	/**
+	 * This constructor draws up a combination of checkboxes and VideoPanels, and needs to recieve a clientmonitor as input
+	 * in order to create the necessary listeners for the checkboxes.
+	 * @param cm (ClientMonitor
+	 */
 	public FrameGUI(ClientMonitor cm) {
 		this.cm = cm;
 		setUIFont(new javax.swing.plaf.FontUIResource("Arial", Font.BOLD, 15));
@@ -75,7 +79,10 @@ public class FrameGUI extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 	}
-
+	/**
+	 * Method to create different fonts in the GUI. The desired font is the input for this method.
+	 * @param f 
+	 */
 	private static void setUIFont(javax.swing.plaf.FontUIResource f) {
 		java.util.Enumeration keys = UIManager.getDefaults().keys();
 		while (keys.hasMoreElements()) {
@@ -85,7 +92,15 @@ public class FrameGUI extends JFrame {
 				UIManager.put(key, f);
 		}
 	}
-
+	/**
+	 * A private method used when creating the checkboxes and placing them in corresponding button groups. Recieves the amount of
+	 * boxes, the names of the boxes in the order they need to be created, the corresponding box vector that the are to be placed in, and lastly
+	 * the corresponding buttongroup that they adhere to.
+	 * @param i
+	 * @param names
+	 * @param boxes
+	 * @param bg
+	 */
 	private void createCheckBoxes(int i, String[] names, JCheckBox boxes[], ButtonGroup bg) {
 		boxes[i] = new JCheckBox(names[i]);
 		boxes[i].setBorder(new EmptyBorder(0, 0, 0, 0));
@@ -95,12 +110,19 @@ public class FrameGUI extends JFrame {
 		bg.add(boxes[i]);
 		jui.add(boxes[i]);
 	}
-
+	/**
+	 * This is a method used by the monitor to update the mode, thus changing which of the checkboxes is highlighted. Input is the
+	 * selected checkbox to highlight.
+	 * @param mode
+	 */
 	public void updateMode(int mode) {
 		System.out.println("Updating GUI mode" + mode);
 		movieCheck[mode - 1].setSelected(true);
 	}
-
+	/**
+	 * A method to indicate which camera caused the recent change in modes. Input is the panel that is specified to blink.
+	 * @param panel
+	 */
 	public void activeCamera(int panel) {
 		switch (panel) {
 		case (1):
@@ -111,7 +133,12 @@ public class FrameGUI extends JFrame {
 			break;
 		}
 	}
-
+	/**
+	 * A method to send images to the corresponding VideoPanel and update the gui. The inputs are the picture that is to be displayed and 
+	 * the frame it is to be displayed on.
+	 * @param p
+	 * @param frame
+	 */
 	public void sendImage(Picture p, int frame) {
 		switch (frame) {
 		case 1:
@@ -124,7 +151,7 @@ public class FrameGUI extends JFrame {
 	}
 
 	public static void main(String[] args) {
-
+		
 		Runnable r = new Runnable() {
 			public void run() {
 				ServerMonitor sm = new ServerMonitor();
---
+++ src/client/Main.java
@@ -0,0 +1,8 @@
+package client;
+
+public class Main {
+	String input;
+	public static void main(String args[]){
+		
+	}
+}
